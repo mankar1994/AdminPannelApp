5cd160a69bca35fdf6b910b588fda4e3e335d870
admin <admin@CHETUIWK1361>
2022-03-23 16:12:58 +0530

    Add project files.

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminPannelApp.Models
+{
+    public class AppDbContext:DbContext
+    {
+        //public AppDbContext() : base() { }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<VerifyAccount> VerifyAccounts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DbConnection.ConnectionStr);
+        }
+
+    }
+}
---
+++ Repository/Interface/IUsers.cs
@@ -0,0 +1,16 @@
+﻿using AdminPannelApp.Utils.Enums;
+using AdminPannelApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminPannelApp.Repository.Interface
+{
+    public interface IUsers
+    {
+        SignInEnum SignIn(SignInModel model);
+        SignUpEnum SignUp(SignUpModel model);
+        bool VerifyAccount(string otp);
+    }
+}
